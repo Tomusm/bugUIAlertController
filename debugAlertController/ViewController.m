7ff62576f4e8c7fe9988c18d70ad0ebea65a6683
Thomas Malval <tomusm@gmail.com>
2014-12-17 23:31:49 +0100

    Reproducing the bug

--- debugAlertController/ViewController.m
+++ debugAlertController/ViewController.m
@@ -8,14 +8,19 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIButton *btnAlert;
 
+- (IBAction)btnAlertTouched:(id)sender;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[self tableView] setDelegate:self];
+    [[self tableView] setDataSource:self];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +28,52 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - UITableViewDataSource
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    [cell.textLabel setText:@"ALERT !"];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self showAlert];
+}
+
+
+#pragma mark - Actions
+
+
+- (IBAction)btnAlertTouched:(id)sender {
+    [self showAlert];
+}
+
+#pragma mark - Show Alert
+
+- (void)showAlert {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Confirmation" message:@"Bonjour" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *no = [UIAlertAction actionWithTitle:@"Non" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *yes = [UIAlertAction actionWithTitle:@"Oui" style:UIAlertActionStyleDefault handler:nil];
+    [alertController addAction:no];
+    [alertController addAction:yes];
+  // If you uncomment the two lines below it will work as expected with the didSelectRow
+  //  dispatch_async(dispatch_get_main_queue(), ^{
+        [self presentViewController:alertController animated:YES completion:^{
+            NSLog(@"Lol");
+        }];
+  //  });
+}
 @end
